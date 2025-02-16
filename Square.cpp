51a6e0cb12d7419475414b3e48d78297f0f38f66
ShaharSolema <shahar0601@cs.colman.ac.il>
2025-02-17 01:46:07 +0200

    Update Square.cpp

--- Square.cpp
+++ Square.cpp
@@ -1,18 +1,20 @@
 #include "Square.h"
-//name=shahar solema
 
-Square::Square(double up, double down , double right, double left , const char* sn):Quad(up, down, right, left, sn) {//constructor. center point = (0,0), name=sn
+Square::Square(double up, double down, double right, double left, const char* sn) 
+    : Quad(up, down, right, left, sn) { // constructor. center point = (0,0), name=sn
 }
-Square::~Square() {	//destructor
-	
+
+Square::~Square() { // destructor
 }
-void Square::shiftX(int x) {	//shifts the center of the square by x (right and left)
-	Point center = getCenter();
-	center.setX(center.getX() + x);
-	setCenter(center);
+
+void Square::shiftX(int x) { // shifts the center of the square by x (right and left)
+    Point center = getCenter();
+    center.setX(center.getX() + x);
+    setCenter(center);
 }
-void Square::shiftY(int y) {	//shifts the center of the square by y (up and down)
-	Point center = getCenter();
-	center.setY(center.getY() + y);
-	setCenter(center);
+
+void Square::shiftY(int y) { // shifts the center of the square by y (up and down)
+    Point center = getCenter();
+    center.setY(center.getY() + y);
+    setCenter(center);
 }
