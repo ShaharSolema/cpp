b4fa34b2e17027c12b32c2773b45492245c0ca13
ShaharSolema <shahar0601@cs.colman.ac.il>
2025-02-17 01:45:12 +0200

    Update Quad.cpp

--- Quad.cpp
+++ Quad.cpp
@@ -1,22 +1,25 @@
 #include "Quad.h"
-//name=shahar solema
 
-Quad::Quad(double up , double down, double right, double left, const char* sn) :m_up(up), m_down(down), m_right(right), m_left(left) {//constructor. center point = (0,0), name=sn
-	setName(sn);
+Quad::Quad(double up, double down, double right, double left, const char* sn) 
+    : m_up(up), m_down(down), m_right(right), m_left(left) { // constructor. center point = (0,0), name=sn
+    setName(sn);
+}
 
+Quad::Quad(const Quad& other) { // copy constructor
+    setShape(other.getName(), other.getCenter());
+    m_up = other.m_up;
+    m_down = other.m_down;
+    m_right = other.m_right;
+    m_left = other.m_left;
 }
-Quad::Quad(const Quad& other) {		//copy constructr
-	setShape(other.getName(), other.getCenter());
-	m_up = other.m_up;
-	m_down = other.m_down;
-	m_right = other.m_right;
-	m_left = other.m_left;
+
+Quad::~Quad() { // destructor
 }
-Quad:: ~Quad() {				//destructor
+
+int Quad::calcArea() const { // returns the quad's area (right*up) (rounded to int)
+    return ((int)(m_right * m_up));
 }
- int Quad::calcArea() const {		//returns the quad's area (right*up) (rounded to int)
-	 return ((int)(m_right * m_up));
- }
- int Quad::calcPerimeter() const {		//returns the quad's Perimeter (the sum of the four ribs) (rounded to int)
-	 return((int)(m_right + m_up + m_left + m_down));
- }
+
+int Quad::calcPerimeter() const { // returns the quad's Perimeter (the sum of the four ribs) (rounded to int)
+    return ((int)(m_right + m_up + m_left + m_down));
+}
